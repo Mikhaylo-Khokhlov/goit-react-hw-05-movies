cb97b719fb3e1162a4bb8ea3ecf95c6f0c72f9c5
Mikhaylo-Khokhlov <mihailhohlov257@gmail.com>
2021-12-01 22:25:15 +0200

    fixes

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -1,16 +1,10 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-axios.defaults.headers.common['Authorization'] =
-  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjI2ZDJjYmI3ZWQ0OGZlM2YzZTcwZDNjNzU3NDYwMiIsInN1YiI6IjYxNTVlZjE0ZGQ3MzFiMDA2MjA0ZTZhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kX2wm4V3sop5bu7gINsXfhNmhSJC64YPPiIbFqqCReM';
-
-// Щоб відображались картинки потрібно при рендерингу додавати
-// ось цей url + картинка, ширину в url можна міняти(там зараз 500)
-
-//  const urlComponent = 'https://image.tmdb.org/t/p/w500';
-//   movies.map((el) => {
-//     <img src={urlComponent + el.poster_path} alt="" />
-//   }
+const API_KEY = '74a210d9545ac2133ea9f3ec78679de8';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+axios.defaults.params = {
+  api_key: API_KEY,
+};
 
 export const getTrendingMovies = async () => {
   try {
@@ -26,9 +20,14 @@ export const getTrendingMovies = async () => {
 
 export const getSearchedMovies = async (query, page = 1) => {
   try {
-    const response = await axios.get(
-      `/search/movie?language=en-US&query=${query}&page=${page}&include_adult=false`,
-    );
+    const response = await axios.get(`/search/movie`, {
+      params: {
+        language: 'en-US',
+        query: query,
+        page: page,
+        include_adult: false,
+      },
+    });
     const data = response.data;
     return data;
   } catch (e) {
@@ -51,15 +50,26 @@ export const getMovieById = async id => {
 };
 
 export const getMovieCast = async movieId => {
-  const queryString = `movie/${movieId}/credits?language=en-US`;
-  const data = await axios.get(queryString);
-
-  return data.data;
+  try {
+    const queryString = `movie/${movieId}/credits?language=en-US`;
+    const data = await axios.get(queryString);
+    return data.data;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    //
+  }
 };
 
 export const getReviews = async movieId => {
-  const queryString = `movie/${movieId}/reviews?language=en-US`;
-  const data = await axios.get(queryString);
+  try {
+    const queryString = `movie/${movieId}/reviews?language=en-US`;
+    const data = await axios.get(queryString);
 
-  return data.data;
+    return data.data;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    //
+  }
 };
